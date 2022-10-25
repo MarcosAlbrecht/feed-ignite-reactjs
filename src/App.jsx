eb44080b359565e0b2abb6d05f0ba7b2d1509906
unknown <marquinhos.beto@hotmail.com>
2022-10-25 11:42:47 -0300

    creating header

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import { Post } from "./Post";
 import { Header } from "./components/Header";
 import './global.css'
 
+import styles from './App.module.css';
+
 function App() {
   return (
     <div>
       <Header></Header>
 
-      <Post 
-        author="Marcos Roberto"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, illum? Perspiciatis eaque sunt recusandae ipsum est doloribus vel veritatis magnam. Sunt eaque eos magnam nisi? Tempora repudiandae reprehenderit facere reiciendis."
-      />
-      <Post 
-        author="Luana"
-        content="Novo post"
-      />
-      
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post 
+            author="Marcos Roberto"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, illum? Perspiciatis eaque sunt recusandae ipsum est doloribus vel veritatis magnam. Sunt eaque eos magnam nisi? Tempora repudiandae reprehenderit facere reiciendis."
+          />
+          <Post 
+            author="Luana"
+            content="Novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
