f9e97ac11f62b2e3be2d2e37bd49d9acaa9a7cf1
unknown <marquinhos.beto@hotmail.com>
2022-12-29 17:11:32 -0300

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import './global.css'
 
 import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -10,9 +11,7 @@ function App() {
       <Header></Header>
 
       <div className={styles.wrapper}>
-        <aside>
-          sidebar
-        </aside>
+        <Sidebar />
         <main>
           <Post 
             author="Marcos Roberto"
